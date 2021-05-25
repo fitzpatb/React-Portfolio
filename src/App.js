e356713f6736261673827b2df14462eb56973954
Brett Fitzpatrick <bfitzpatrick28@gmail.com>
2021-05-24 18:18:29 -0600

    basic page rendering up and running just need to work on server and data flow

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
 import React from "react";
-import HelloDiv from "./components/HelloDiv";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Nav from "./components/Nav";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
+import Portfolio from "./pages/Portfolio";
 
 function App() {
-  return <HelloDiv />;
+  return (
+    <Router>
+      <div>
+        <Nav />
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/contact">
+          <Contact />
+        </Route>
+        <Route exact path="/portfolio">
+          <Portfolio />
+        </Route>
+        <Footer />
+      </div>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <p class="white">Copyright Brett Fitzpatrick 2021</p>
+    </footer>
+  )
+}
+
+export default Footer;
