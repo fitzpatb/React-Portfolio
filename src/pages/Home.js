ea38ad6ed44d7d6d4e13e33c93baa99cdfda27a4
Brett Fitzpatrick <bfitzpatrick28@gmail.com>
2021-06-01 18:28:26 -0600

    working on github pages

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,7 +11,7 @@ function Home() {
         </div>
         <div className="row border-top">
           <div className="col-12">
-            <img className="about" src="assets/images/me.jpeg" alt="Brett Fitzpatrick" width="250px" height="250px" />
+            <img className="about" src="images/me.jpeg" alt="Brett Fitzpatrick" width="250px" height="250px" />
             <p>
               My name is Brett Fitzpatrick. I have recently found a passion for technology, computers and coding. I used to be in sales and slowly found out that sales was simply not for me. I have been coding since around July and since then I have built a basic level knowledge of HTML, CSS and JavaScript. I really enjoy getting to be creative and solve problems and I have found that learning how to code fits my personality and lifestyle. I look forward to expanding my knowledge and becoming a pro with HTML, CSS and Javascript.
             </p>
@@ -22,7 +22,7 @@ function Home() {
             <h4 className="slide"><i>Education/Experience</i></h4>
           </div>
           <div className="col-12">
-            <img className="family" src="assets/images/Family.png" alt="Me and my family" width="250px" height="250px" />
+            <img className="family" src="images/Family.png" alt="Me and my family" width="250px" height="250px" />
 
             <p>
               I attended Chadwick Prepatory School in Palos Verdes, CA for my high school career. From there I choose to attend Purdue University in Indiana where I graduated with a degree in Mass Communications and a minor in Management. After working various jobs as a Lift Operator at Vail Resorts, Management Assistant at Enterprise, and a Business Development Representative at Granicus I decdided to pursue a career in front end development.
